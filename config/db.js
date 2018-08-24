54ea17b4a8f23dfb0ae20eb792889c4c5b34cb0e
Bana <Rahul.Bana@landisgyr.com>
2018-08-24 16:00:31 +0530

    code updated 1

--- config/db.js
+++ config/db.js
@@ -1,14 +1,15 @@
 const mysql = require('mysql');
 const connection  = require('express-myconnection');
+const { DBHOST, DBNAME, DBPWD, DBUSER, DBPORT } = process.env;
 
 module.exports = (app) => {
   app.use(
     connection(mysql, {  
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'customer',
-      port: 3306
+      host: DBHOST,
+      database: DBNAME,
+      port: DBPORT,
+      user: DBUSER,
+      password: DBPWD,
     },'pool') //or single
   );
 }
